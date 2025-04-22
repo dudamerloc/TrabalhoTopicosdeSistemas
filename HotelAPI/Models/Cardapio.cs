46046372b6db7f8a0ade42615704ff6f7387ae7e
dropexz <pedrovaleriano75@gmail.com>
2025-04-22 14:29:08 -0300

    Adicionando, Criado e testando API do Cardápio de bebidas

---
+++ HotelAPI/Models/Cardapio.cs
@@ -0,0 +1,10 @@
+
+
+    public class Bebida
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public bool Alcoolica { get; set; } // Define se é alcoólica ou não
+    }
+
